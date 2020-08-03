b84559f91995033f73108c54ce5eb73ebbc4b198
Pavel Lichmaniuk <Pavel Lichmaniuk>
2020-08-03 18:04:02 +0300

    add errors to console

--- task2.js
+++ task2.js
@@ -5,4 +5,4 @@ const csvFilePath = "./csv/task-2-data.csv"
 const readStream = fs.createReadStream(csvFilePath);
 const writeStream = fs.createWriteStream("./task-2-Result.txt", "utf8");
 
-readStream.pipe(csvtojson()).pipe(writeStream);
+readStream.pipe(csvtojson()).pipe(writeStream).on('error', (err) => process.stderr.write(err));
