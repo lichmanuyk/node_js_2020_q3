d7ae6c03c515cc436bea52ad4fcff93da2e5a3eb
Pavel Lichmaniuk <Pavel Lichmaniuk>
2020-08-03 13:53:47 +0300

    add task 2

---
+++ task2.js
@@ -0,0 +1,8 @@
+const csvtojson = require("csvtojson");
+const fs = require("fs");
+
+const csvFilePath = "./csv/task-2-data.csv"
+const readStream = fs.createReadStream(csvFilePath);
+const writeStream = fs.createWriteStream("./task-2-Result.txt", "utf8");
+
+readStream.pipe(csvtojson()).pipe(writeStream);
